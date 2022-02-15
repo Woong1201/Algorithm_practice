5e3a54b16a39e711d3fa8df25800621a6e7c6601
woong <duddnd170@ajou.ac.kr>
2022-02-15 17:18:25 +0900

    단계별로 풀어보기

---
+++ 입출력과 사칙연산.py
@@ -0,0 +1,64 @@
+#2557
+print("Hello World!")
+
+#10718
+print("강한친구 대한육군\n\
+강한친구 대한육군")
+
+#10171
+print("\    /\\\n\
+ )  ( ')\n\
+(  /  )\n\
+ \(__)|")
+
+#10172
+print("|\\_/|\n\
+|q p|   /}\n\
+( 0 )\"\"\"\\\n\
+|\"^\"`    |\n\
+||_/=\\\\__|")
+
+#1000
+a, b = map(int, input().split())
+print(a+b)
+
+#1001
+a, b = map(int, input().split())
+print(a-b)
+
+#10098
+a, b = map(int, input().split())
+print(a*b)
+
+#1008
+a, b = map(int, input().split())
+print(a/b)
+
+#10869
+a, b = map(int, input().split())
+print(a+b)
+print(a-b)
+print(a*b)
+print(a//b)
+print(a%b)
+
+#10926
+print(input() + "??!")
+
+#18108
+print(int(input())-543)
+
+#10430
+A, B, C = map(int, input().split())
+print((A+B)%C)
+print(((A%C) + (B%C))%C)
+print((A*B)%C)
+print(((A%C) * (B%C))%C)
+
+#2588
+a = int(input())
+b = int(input())
+print(a*(b%10))
+print(a*(b%100-b%10)//10)
+print(a*(b//100))
+print(a*b)
