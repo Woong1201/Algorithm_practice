efb85cda1568496fdef3e3695671e4a71140513a
woong <duddnd170@ajou.ac.kr>
2022-04-07 16:52:41 +0900

    투 포인터

--- baekjoon/01806 부분합/01806 부분합.py
+++ baekjoon/01806 부분합/01806 부분합.py
@@ -2,27 +2,27 @@ import sys
 
 N, S = map(int, input().split())
 array = list(map(int, sys.stdin.readline().split()))
-start, end, new_len, sum_array = 0, 0, 0, 0
+start, end, array_len, sum_array = 0, 0, 0, 0
 num_len = N + 1
 
 while num_len - 1:
-    if new_len > num_len:
+    if array_len > num_len:
         sum_array -= array[start]
         start += 1
-        new_len -= 1
+        array_len -= 1
         continue
     
     if sum_array >= S:
-        num_len = min(new_len, num_len)
+        num_len = min(array_len, num_len)
         sum_array -= array[start]
         start += 1
-        new_len -= 1
+        array_len -= 1
     elif end == N:
         break 
     else:
         sum_array += array[end]
         end += 1
-        new_len += 1
+        array_len += 1
 
 if num_len == N + 1:
     print(0)
